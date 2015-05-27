7c1efe1e482d3ee5c9bfe9d24cd4c80f3843687d
Timotius Sitorus <timsitorus1@gmail.com>
2015-05-27 19:05:45 -0400

    added table header

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ var env = require('./vendor/electron_boilerplate/env_config');
 var windowStateKeeper = require('./vendor/electron_boilerplate/window_state');
 
 var mainWindow;
-var DEBUG = false;
+var DEBUG = true;
 
 // Preserver of the window size and position between app launches.
 var mainWindowState = windowStateKeeper('main', {
