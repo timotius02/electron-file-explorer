3393edf5bbd0b550842604406b16ab2ea7e2c3b7
Timotius Sitorus <timsitorus1@gmail.com>
2015-05-28 12:18:30 -0400

    mapping success

--- app/app.js
+++ app/app.js
@@ -1,22 +1,96 @@
 var fs = require('fs');
 var shell = require('shell');
 var React = require('react');
+var path = require('path');
 
-import { FilesLayout } from './client/components';
-var isWindows = process.platform === 'win32'
+//import * as collections from './client/components';
 
 // window.env contains data from config/env_XXX.json file.
 var envName = window.env.name;
 
-var getHome = function() {
-  return ( isWindows ? process.env.USERPROFILE: process.env.HOME);
-}
+var singleFile = {
+    fileName: "Example",
+    fileSize: "20kb",
+    fileType: "JPEG Image",
+    fileModified: "Today 11:21pm"
+};
 
-var files = fs.readdirSync(getHome());
+// var filesDataStore = {
+// 	filesData: [],
+// 	addFiles: function(file){
+// 		this.filesData.push(file);
+// 	}
+// }
 
-// files.map(function(file){
-// 	if(file.substring(0, 1) !== '.')
-// 		document.write(file + '<br>');
-// });
+var isWindows = process.platform === 'win32'
+var getHome =  isWindows ? process.env.USERPROFILE: process.env.HOME;
 
-React.render(<FilesLayout />, document.getElementById('files-container'));
+var files = fs.readdirSync(getHome);
+var filesData = [];
+
+files.map(function(file) {
+    if (file.substring(0, 1) !== '.') {
+
+        fs.stat(path.join(getHome, file), function(err, stats) {
+            if (err)
+                console.log(err);
+
+			filesData.push({
+                fileName: file,
+                fileSize: stats.size,
+                fileType: stats.isFile() ? "File" : "Directory",
+                fileModified: stats.mtime.toLocaleString()
+            });
+        });
+    }
+
+});
+
+var File = React.createClass({
+	render: function(){
+		var size = "";
+		if ( Math.floor(this.props.fileSize / 1000000) !== 0) // Megabytes
+		    size = Math.floor(this.props.fileSize / 1000000) + " M";
+		else if (Math.floor(this.props.fileSize / 1000) !== 0)
+		    size = Math.floor(this.props.fileSize / 1000) + " K"; // Kilobytes
+		else
+		    size = this.props.fileSize + " B"; // Bytes
+
+		return (
+			<div className="files">
+	            <div className="filename">{this.props.fileName}</div>
+	            <div className="filesize">{size}</div>
+	            <div className="filetype">{this.props.fileType}</div>
+	            <div className="filemodified">{this.props.fileModified}</div>
+	        </div>
+		)
+	}
+});
+
+
+var FilesLayout = React.createClass({
+	getInitialState: function(){
+		return {filesData: filesData};
+	},
+	componentDidMount: function(){
+		this.setState({filesData: filesData });
+	},
+	render: function() {
+		console.log(filesData);
+		var fileList = this.state.filesData.map(function(fileInfo){
+			return <File {...fileInfo} />
+		});
+
+		return (
+			<div id="files-container">
+				{fileList}
+			</div>		
+		)
+	}
+});
+//Places render call behind every else in the call stack
+setTimeout(function(){
+	React.render(<FilesLayout />, document.getElementById('files-container'));
+}, 1);
+
+
--- app/client/components.js
+++ app/client/components.js
@@ -1,17 +1,51 @@
 var React = require('react');
+var fs = require('fs');
+var path = require('path');
 
-var singleFile = {
-    fileName: "Example",
-    fileSize: "20kb",
-    fileType: "JPEG Image",
-    fileModified: "Today 11:21pm"
-};
+// var singleFile = {
+//     fileName: "Example",
+//     fileSize: "20kb",
+//     fileType: "JPEG Image",
+//     fileModified: "Today 11:21pm"
+// };
 
-var filesList = [];
-var i = 0;
-while(i++ < 8) {
-	filesList.push(singleFile);
-}
+// var filesList = [];
+// var i = 0;
+// while(i++ < 8) {
+// 	filesList.push(singleFile);
+// }
+var isWindows = process.platform === 'win32'
+var getHome =  isWindows ? process.env.USERPROFILE: process.env.HOME;
+
+var files = fs.readdirSync(getHome);
+var filesData = [];
+
+// files.map(function(file){
+	for(var i = 0; i <= files.size; i++){
+		console.log(i);
+		if (i === files.size){
+			callback();
+			console.log('Hello World');
+			break;
+		}
+
+		var file = files[i];
+		if(file.substring(0, 1) !== '.'){
+			fs.stat(path.join(getHome, file), function(err, stats){
+				if(err)
+					console.log(err);
+
+				var fileData = {
+					fileName: name,
+					fileType: stats.isFile() ? "File": "Directory",
+					fileSize: stats.size,
+					fileModified: stats.mtime.toLocaleString()
+				}
+				filesData.push(fileData);
+			});
+		}
+	}
+// });
 
 var File = React.createClass({
 	render: function(){
@@ -26,9 +60,10 @@ var File = React.createClass({
 	}
 });
 
-export var FilesLayout = React.createClass({
+
+var FilesLayout = React.createClass({
 	getInitialState: function(){
-		return {files: filesList};
+		return {files: filesData};
 	},
 	render: function() {
 		var files = this.state.files.map(function(fileInfo){
@@ -41,3 +76,8 @@ export var FilesLayout = React.createClass({
 		)
 	}
 });
+
+var callback = function() {
+
+	React.render(<FilesLayout />, document.getElementById('files-container'));
+}
